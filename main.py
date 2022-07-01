4599f238ed600cfd8d5ec61ad7ec7fbd45b5dc2f
Sannidhya <sannidhyamalpani95@gmail.com>
2022-07-01 17:47:22 +0530

    Init profiniy calculator

---
+++ main.py
@@ -0,0 +1,27 @@
+
+import profiniy_calculator as pc
+
+racial_slurs = {
+    ('hate', 'X') : 1,
+    ('Y', 'back', 'country') : 2,
+    ('D', 'love' ,'E') : 1.5,
+    ('M', 'G', 'love') : 1,
+    ('X', 'go', 'C') : 2.5
+}
+
+path = "resouces/sampleTweets.csv"
+
+def profinity_checker():
+
+    profin_cal = pc.Profinity_calculator(racial_slurs)
+
+    csv = profin_cal.read_csv(path)
+
+    profin_score = profin_cal.profin_calculator(csv)
+
+    profin_ranks = profin_cal.profin_ranker(profin_score)
+
+    profin_cal.print_results(csv,profin_ranks)
+
+
+profinity_checker()
---
+++ profiniy_calculator.py
@@ -0,0 +1,65 @@
+import csv
+import re
+
+class Profinity_calculator:
+    profin_words = {}
+    def __init__(self, profin_words):
+        self.profin_words = profin_words
+
+    def read_csv(self, path):
+        csv_list = []
+        if path != None and '.csv' == path[-4:]:
+            with open(path, mode ='r')as file:
+                csvFile = csv.reader(file)
+                for line in csvFile:
+                    csv_list.append(line)
+        return csv_list[1:]
+
+
+    def pattern_matching(self, words, index, sentence):
+        search_list = re.split(words[index], sentence)[1:]
+        if search_list == []:
+            return 0
+        index += 1
+        if len(words) == index:
+            return 1
+        for sentence in search_list:
+            if self.pattern_matching(words, index, sentence) == 1:
+                return 1
+        return 0
+
+
+    def profin_calculator(self, csv):
+        if csv == [] or csv == None:
+            return 0
+        scores = []
+        for row in csv:
+            score = 0
+            for words, profin_score in self.profin_words.items():
+                if self.pattern_matching(words, 0, row[1]) == 1:
+                    score += profin_score
+            scores.append(score)
+
+        return scores
+
+    def profin_ranker(self, scores):
+        remarks = []
+        for score in scores:
+            if score == 0:
+                remarks.append('None')
+            if score > 0 and score <= 1:
+                remarks.append('Low')
+            if score > 1 and score <= 2:
+                remarks.append('Moderate')
+            if score > 2:
+                remarks.append('Extreme')
+        return remarks
+
+    def print_results(self, csv, rank):
+        print('User', '\t', 'Tweet', '\t', 'rank')
+        for i in range(0,len(csv)):
+            print(csv[i][0], '\t', csv[i][1], '\t', rank[i])
+
+
+
+
